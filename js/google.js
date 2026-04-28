162018002fc7ef7d280914d9afcec05669169eda
Lance Adrian de Pasion <lance.depasion@deped.gov.ph>
2026-04-28 17:16:43 +0800

    Test bypass validate token 3

--- js/google.js
+++ js/google.js
@@ -1,28 +1,26 @@
 // STAGING - Google Auth Bypass
 function handleClientLoad() {
-  setTimeout(function() {
-    var authorizeButton = $$('button#authorize-button')[0];
-    var continueButton = $$('button#continue-button')[0];
-    var signoutButton = $$('a#signout-button')[0];
-    var loginheading = $$('#login-heading')[0];
+  var authorizeButton = $$('button#authorize-button')[0];
+  var continueButton = $$('button#continue-button')[0];
+  var signoutButton = $$('a#signout-button')[0];
+  var loginheading = $$('#login-heading')[0];
 
-    if (!authorizeButton) return;
+  if (!authorizeButton) return;
 
-    loginheading.textContent = 'MOOE Login';
-    authorizeButton.style.display = 'block';
-    continueButton.style.display = 'none';
-    signoutButton.style.display = 'none';
+  loginheading.textContent = 'MOOE Login';
+  authorizeButton.style.display = 'block';
+  continueButton.style.display = 'none';
+  signoutButton.style.display = 'none';
 
-    authorizeButton.onclick = function() {
-      app.data.googleprofile = {
-        getEmail: function() { return 'lance.depasion@deped.gov.ph'; },
-        getId: function() { return '000000000000000000000'; },
-        getFamilyName: function() { return 'De Pasion'; },
-        getGivenName: function() { return 'Lance Adrian'; },
-        getImageUrl: function() { return ''; }
-      };
-      app.data.googletoken = 'staging-bypass-token';
-      app.methods.signin();
+  authorizeButton.onclick = function() {
+    app.data.googleprofile = {
+      getEmail: function() { return 'lance.depasion@deped.gov.ph'; },
+      getId: function() { return '000000000000000000000'; },
+      getFamilyName: function() { return 'De Pasion'; },
+      getGivenName: function() { return 'Lance Adrian'; },
+      getImageUrl: function() { return ''; }
     };
-  }, 1000);
+    app.data.googletoken = 'staging-bypass-token';
+    app.methods.signin();
+  };
 }
